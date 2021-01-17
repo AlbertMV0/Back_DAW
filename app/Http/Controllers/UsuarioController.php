130b346429c52f3dee2f4722cd5feb8cb514baf9
Albert Morenilla <48762638+PrimalPres@users.noreply.github.com>
2021-01-17 21:19:47 +0100

    Devolver todos los usuarios

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Usuario;
+
+class UsuarioController extends Controller
+{
+   public function getAllUsuarios(){
+
+    return response()->json(Usuario::get(),200);
+   }
+}
