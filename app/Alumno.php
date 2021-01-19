c16e0a0549b82b18be446f5faa6a0ec51a553970
Albert Morenilla <48762638+PrimalPres@users.noreply.github.com>
2021-01-19 01:37:25 +0100

    Creado relaciones

--- app/Alumno.php
+++ app/Alumno.php
@@ -6,6 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alumno extends Model
 {
-    protected $table = 'alumno';
+    protected $table = 'alumno';    
     protected $primaryKey='id_alumno';
+
+    public function comentarios()
+    {
+        return $this->hasMany('App\FreeTimesVideo');
+    }
+
+    public function padre()
+    {
+        return $this->hasMany('App\Alumno_padre');
+    }
+
+    public function clase()
+    {
+        return $this->hasOne('App\Clase');
+    }
 }
