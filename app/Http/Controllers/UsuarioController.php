00cdac05309397a2dd4001390a7741af86aefbe7
Albert Morenilla <48762638+PrimalPres@users.noreply.github.com>
2021-01-21 13:56:25 +0100

    Configuracion passport

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -8,7 +8,7 @@ use App\Usuario;
 class UsuarioController extends Controller
 {
    public function getAllUsuarios(){
-
-    return response()->json(Usuario::get(),200);
+     // return response()->json(Usuario::get(),200);
+    return Usuario::all();
    }
 }
--- app/Usuario.php
+++ app/Usuario.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
-class Usuario extends Model
+class Usuario extends Authenticatable  
 {
 use Notifiable, HasApiTokens;
+
+    
+//protected $fillable =['nombre','apellidos','email','password']; 
 
     protected $table = 'usuario';
     protected $primaryKey='id_usuario';
