fc85ae4d0fd30ce11284dc55f71af17fecc4b22d
Albert Morenilla <48762638+PrimalPres@users.noreply.github.com>
2021-01-21 02:55:40 +0100

    route usuarios y PASSPORT

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('usuarios','UsuarioController@getAllUsuarios');
+
+//rousources para generar 
+Route::resource('usuarios','UsuarioController@getAllUsuarios');
