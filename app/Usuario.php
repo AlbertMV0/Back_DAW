33a339b7c6ada9e8a1a4dbabe18f9844f89c4222
Albert Morenilla <48762638+PrimalPres@users.noreply.github.com>
2021-01-21 03:15:46 +0100

    Configurando la autentificacion

--- app/Usuario.php
+++ app/Usuario.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
+use Notifiable, HasApiTokens;
+
     protected $table = 'usuario';
     protected $primaryKey='id_usuario';
 }
